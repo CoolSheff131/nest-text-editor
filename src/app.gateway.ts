f657461e5abd36529d4f243b484d4522c5e04c78
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-03-11 21:57:49 +0800

    комнаты сокетов

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -7,8 +7,20 @@ export class AppGateway {
   @WebSocketServer() wss: Server
 
   @SubscribeMessage('msgToServer')
-  handleMessage(client: Socket, payload: any): void {
-    this.wss.emit('msgFromServer', payload)
+  handleMessage(client: Socket, message: {textId: string, text: string}): void {
+    this.wss.to(message.textId).emit('msgFromServer', message.text)
     //return {event: 'a',data: 'Hello world'};
+  }
+
+  @SubscribeMessage('joinRoom')
+  handleJoin(client: Socket, message: {textId: string, user: string}){
+    client.join(message.textId)
+    this.wss.to(message.textId).emit('joinedRoom', `${message.user} joined`)
+  }
+
+  @SubscribeMessage('leaveRoom')
+  handleLeft(client: Socket, message: {textId: string, user: string}){
+    client.leave(message.textId)
+    this.wss.to(message.textId).emit('leftRoom', `${message.user} left`)
   }
 }
